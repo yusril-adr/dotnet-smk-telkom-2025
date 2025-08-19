e2e9b524e9db7e01525bfaf0a8867c26428e3181
Yusril Adriansyah Putra <yusriladr.37@gmail.com>
2025-08-19 15:04:16 +0700

    feat: :building_construction: Made post query parameter

---
+++ Dtos/Parameters/PostQueryParameter.cs
@@ -0,0 +1,7 @@
+using dotnet_smk_telkom_2025.Infrastructure.Dtos;
+
+namespace dotnet_smk_telkom_2025.Dtos.Parameters;
+
+public class PostQueryParameter : QueryParameter
+{
+}
---
+++ Infrastructure/Dtos/QueryParameter.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace dotnet_smk_telkom_2025.Infrastructure.Dtos;
+
+public class QueryParameter
+{
+  public QueryParameter()
+  {
+    PerPage = 10;
+    Page = 1;
+  }
+
+  [Range(1, 100)]
+  public int PerPage { get; set; }
+
+  [Range(1, int.MaxValue)]
+  public int Page { get; set; }
+}
